9ee1a1242a15f3b920e1e1c8e8d86937a358579a
fweng <fanweng02@gmail.com>
2021-10-16 22:25:45 -0400

    Add extra buttons and set their constraints/properties.

--- build-your-first-android-app-in-java/app/src/main/java/com/example/myfirstapp/FirstFragment.java
+++ build-your-first-android-app-in-java/app/src/main/java/com/example/myfirstapp/FirstFragment.java
@@ -29,7 +29,7 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        binding.randomButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
